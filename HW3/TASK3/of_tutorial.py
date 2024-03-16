58984ec38a9135019d6f8420aa3aad7d8b3e5981
PeterZhu <74828967+soy-crypto@users.noreply.github.com>
2024-03-16 21:58:38 +0000

    0316

--- HW3/TASK3/of_tutorial.py
+++ HW3/TASK3/of_tutorial.py
@@ -63,20 +63,25 @@ class Tutorial (object):
     self.connection.send(msg)
 
 
-  def act_like_hub (self, packet, packet_in):
-    """
-    Implement hub-like behavior -- send all packets to all ports besides
-    the input port.
-    """
+  def act_like_switch (self, packet, packet_in):
+    
+    # Learn the port for the source MAC
+    # print("Src: ",str(packet.src),":", packet_in.in_port,"Dst:", str(packet.dst))
+    if packet.src not in self.mac_to_port:
+      print("Learning that " + str(packet.src) + " is attached at port " +
+      str(packet_in.in_port))
+      self.mac_to_port[packet.src] = packet_in.in_port
 
-    # We want to output to all ports -- we do that using the special
-    # OFPP_ALL port as the output port.  (We could have also used
-    # OFPP_FLOOD.)
-    self.resend_packet(packet_in, of.OFPP_ALL)
-
-    # Note that if we didn't get a valid buffer_id, a slightly better
-    # implementation would check that we got the full data before
-    # sending it (len(packet_in.data) should be == packet_in.total_len)).
+    # if the port associated with the destination MAC of the packet is known:
+    if packet.dst in self.mac_to_port:
+      # Send packet out the associated port
+      print(str(packet.dst) + " destination known. only send message to it")
+      self.resend_packet(packet_in, self.mac_to_port[packet.dst])
+    else:
+      # Flood the packet out everything but the input port
+      # This part looks familiar, right?
+      print(str(packet.dst) + " not known, resend to everybody")
+      self.resend_packet(packet_in, of.OFPP_ALL)
 
 
   def act_like_switch (self, packet, packet_in):
---
+++ HW3/TASK3/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#!/bin/sh
+
+for i in `seq 1 100`;
+do
+    echo h1 ping h2 >> output1.txt
+done
+
+for i in `seq 1 100`;
+do
+    h1 ping h8 >> > output2.txt
+ done 
+
